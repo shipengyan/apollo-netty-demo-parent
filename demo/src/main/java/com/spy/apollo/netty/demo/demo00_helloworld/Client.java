80662de197a87fe64d93cdee536faf01b6850e00
shi.pengyan <spy19881221@163.com>
2017-07-12 21:58:37 +0800

    helloworld

---
+++ demo/src/main/java/com/spy/apollo/netty/demo/demo00_helloworld/Client.java
@@ -0,0 +1,90 @@
+package com.spy.apollo.netty.demo.demo00_helloworld;
+
+import com.spy.apollo.netty.demo.common.Const;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.codec.LengthFieldPrepender;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.InetSocketAddress;
+
+/**
+ * 模块名
+ *
+ * @author shi.pengyan
+ * @version 1.0 2017-07-11 21:58
+ * @since 1.0
+ */
+@Slf4j
+public class Client {
+    @Setter
+    private String host;
+    @Setter
+    private int    port;
+
+    private EventLoopGroup group;
+    private Bootstrap      bootstrap;
+
+    @Setter
+    private ClientHandler clientHandler;
+
+    public static void main(String[] args) throws InterruptedException {
+        Client client = new Client();
+
+        client.setHost(Const.HOST);
+        client.setPort(Const.PORT);
+        ClientHandler handler = new ClientHandler();
+
+        client.setClientHandler(handler);
+
+
+        client.start();
+
+    }
+
+    public void start() throws InterruptedException {
+        group = new NioEventLoopGroup(1);
+
+        try {
+            bootstrap = new Bootstrap();
+            bootstrap.group(group)
+                     .channel(NioSocketChannel.class)
+                     .remoteAddress(new InetSocketAddress(host, port))
+                     .option(ChannelOption.SO_KEEPALIVE, true)
+                     .option(ChannelOption.TCP_NODELAY, true)
+                     .handler(new LoggingHandler(LogLevel.INFO))
+                     .handler(new ChannelInitializer<SocketChannel>() {
+                         @Override
+                         public void initChannel(SocketChannel ch) throws Exception {
+                             //ch.pipeline().addLast("timeout", new IdleStateHandler(readerIdleTime, writerIdleTime, allIdleTime, TimeUnit.SECONDS));
+
+
+                             ch.pipeline()
+
+                               .addLast("encoder", new LengthFieldPrepender(4, false))
+                               .addLast("decoder", new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 4, 0, 4))
+
+                               .addLast("clientHandler", clientHandler);
+                         }
+                     });
+
+            ChannelFuture f = bootstrap.connect().sync();        //6
+
+            f.channel().closeFuture().sync();            //7
+        } finally {
+            group.shutdownGracefully().sync();            //8
+        }
+
+    }
+
+}
---
+++ demo/src/main/java/com/spy/apollo/netty/demo/demo00_helloworld/ClientHandler.java
@@ -0,0 +1,47 @@
+package com.spy.apollo.netty.demo.demo00_helloworld;
+
+import com.alibaba.fastjson.JSON;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 模块名
+ *
+ * @author shi.pengyan
+ * @version 1.0 2017-07-11 21:58
+ * @since 1.0
+ */
+@Slf4j
+public class ClientHandler extends ChannelInboundHandlerAdapter {
+
+    private ChannelHandlerContext ctx;
+
+    public void sendMsg(Object obj) {
+        if (ctx != null && ctx.channel().isActive()) {
+            log.debug("client send msg {}", obj);
+            ctx.writeAndFlush(Unpooled.buffer().writeBytes(JSON.toJSONString(obj).getBytes(CharsetUtil.UTF_8)));
+        }
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        this.ctx = ctx;
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        log.debug("channel active");
+        sendMsg("hello world!");
+    }
+
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
