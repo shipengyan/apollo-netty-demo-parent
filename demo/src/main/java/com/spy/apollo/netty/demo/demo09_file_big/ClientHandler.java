414a9d0b2307cc8ca977924c1f22feb2ff912849
shi.pengyan <spy19881221@163.com>
2017-07-19 09:06:38 +0800

    big file //TODO

---
+++ demo/src/main/java/com/spy/apollo/netty/demo/demo09_file_big/ClientHandler.java
@@ -0,0 +1,102 @@
+package com.spy.apollo.netty.demo.demo09_file_big;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.spy.apollo.netty.demo.demo08_custom_event.event.CustomEvent;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.time.DateFormatUtils;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 模块名
+ *
+ * @author shi.pengyan
+ * @version 1.0 2017-07-11 21:58
+ * @since 1.0
+ */
+@Slf4j
+public class ClientHandler extends ChannelInboundHandlerAdapter {
+
+    private ChannelHandlerContext ctx;
+
+    public void sendMsg(Object obj) {
+        if (ctx != null && ctx.channel().isActive()) {
+            log.debug("client send msg {}", obj);
+            ctx.writeAndFlush(Unpooled.buffer().writeBytes(JSON.toJSONString(obj).getBytes(CharsetUtil.UTF_8)));
+        }
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        this.ctx = ctx;
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        log.debug("channel active");
+
+        ctx.channel().eventLoop().scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                JSONObject jsonObj = new JSONObject();
+
+                jsonObj.put("action", "file");// request file
+
+                sendMsg(jsonObj);
+            }
+        }, 2, 5, TimeUnit.SECONDS);
+        sendMsg("hello world!");
+    }
+
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        log.debug("channel read");
+
+        String filename = DateFormatUtils.format(new Date(), "yyyyMMddHHmmss");
+        String filePath = String.format("c:/biz%s.txt", filename);
+
+        FileOutputStream fos = new FileOutputStream(filePath, true);
+//      BufferedOutputStream bufferedOutputStream=new BufferedOutputStream(fos);
+        ByteBuf buf   = (ByteBuf) msg;
+        byte[]  bytes = new byte[buf.readableBytes()];
+        buf.readBytes(bytes);
+        log.debug("receive content lenght={}", bytes.length);
+
+        try {
+            fos.write(bytes);
+            fos.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        if (evt instanceof CustomEvent) {
+
+            CustomEvent customEvent = (CustomEvent) evt;
+
+            log.debug("custom data={}", customEvent.getData());
+        }
+
+
+    }
+}
---
+++ demo/src/main/java/com/spy/apollo/netty/demo/demo09_file_big/ServerHandler.java
@@ -0,0 +1,84 @@
+package com.spy.apollo.netty.demo.demo09_file_big;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+import io.netty.util.ReferenceCountUtil;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+
+/**
+ * 模块名
+ *
+ * @author shi.pengyan
+ * @version 1.0 2017-07-11 21:34
+ * @since 1.0
+ */
+@Slf4j
+public class ServerHandler extends ChannelInboundHandlerAdapter {
+    private ChannelHandlerContext ctx;
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        this.ctx = ctx;
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf byteBuf = (ByteBuf) msg;
+
+        String readStr = byteBuf.toString(CharsetUtil.UTF_8);
+        log.debug("Server received: {}", readStr);
+
+        //TODO
+        log.debug("channel is writable={}", ctx.channel().isWritable());
+
+        ReferenceCountUtil.release(msg);
+
+        // check whether is get one file
+        JSONObject jsonObject;
+        try {
+            jsonObject = JSON.parseObject(readStr);
+        } catch (Exception e) {
+            log.error("invalid json", e);
+            return;
+        }
+
+        String action = jsonObject.getString("action");
+        if ("file".equalsIgnoreCase(action)) {
+            File            file = new File("c:/biz.txt"); //TODO you should change it
+            FileInputStream in   = new FileInputStream(file);
+
+            byte[] buffer = new byte[1024 * 100];
+
+            BufferedInputStream bis = new BufferedInputStream(in);
+            int                 n   = 0;
+            while ((n = in.read(buffer)) != -1) {
+                byteBuf = Unpooled.buffer(buffer.length);
+                //这里读取到多少，就发送多少，是为了防止最后一次读取没法满填充buffer，
+                //导致将buffer中的处于尾部的上一次遗留数据也发送走
+                byteBuf.writeBytes(buffer, 0, n);
+                ctx.write(byteBuf);
+                byteBuf.clear();
+            }
+            ctx.flush();
+            log.info("send file over");
+        }
+
+
+    }
+
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
